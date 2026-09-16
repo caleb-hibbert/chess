2fb80ab8284bcb6cce97e50a62ae3827e8445763
Caleb <calebhibbert07@gmail.com>
2026-09-15 22:00:09 -0600

    Working through pawn movement calculator, ironing out attack logic

--- shared/src/main/java/chess/PawnMoveCalculator.java
+++ shared/src/main/java/chess/PawnMoveCalculator.java
@@ -13,54 +13,55 @@ public class PawnMoveCalculator implements PieceMovesCalculator{
 
         int row = position.getRow();
         int col = position.getColumn();
-
-
+        int moveDirection = 1;
+        int[] attackPositionLeft = {1, -1};
+        int[] attackPositionRight = {1, 1};
+        int startingRow = -1;
+        int promotionRow = -1;
 
         if (piceColor == ChessGame.TeamColor.WHITE){
-            if (row == 2){
-                //check 2 squares in front + attack option
-            }
-            else if (row == 8){
-                // promote
-            }
-            else{
-                //check 1 square in front + attack option
-            }
-
-
+            moveDirection = 1;
+            attackPositionLeft = new int[] {1, -1};
+            attackPositionRight = new int[] {1, 1};
+            startingRow = 2;
+            promotionRow = 8;
         }
         else if (piceColor == ChessGame.TeamColor.BLACK){
-
-
+            moveDirection = -1;
+            attackPositionLeft = new int[] {-1, -1};// the "left" attack will go left from White's POV, and right from Black's POV, towards column 1
+            attackPositionRight = new int[] {-1, 11};
+            startingRow = 7;
+            promotionRow = 1;
         }
 
 
 
-        int[] white_forward = {1,0};
-        int[] black_forward = {-1,0};
+        // Movement checks
+        if (row >= 2 && row <= 7) {// make sure we're at least 1 spot from the edge so moving forward doesn't take us off the board
+            if (board.getPiece(new ChessPosition(row + moveDirection, col)) == null) {// if first space in front clear, that's a valid move
+                possibleMoves.add(new ChessMove(new ChessPosition(row, col), new ChessPosition(row + moveDirection, col), null));
 
-
-        int[][] movableDirections = {white_forward,};
-        //TODO: Account for whether the piece has moved or not to determine if add +2 first move to possiblemoves
-        //TODO: Account for what side the piece is on
-        //TODO: Account for pieces in front stopping movement, pieces diagonal allowing for attack+move
-
-        for (int[] directions : movableDirections){
-            int row = position.getRow() + directions[0];
-            int col = position.getColumn() + directions[1];// increment row + col in each direction
-
-            while (row >= 1 && col >= 1 && row <= 8 && col <= 8){// while in bounds
-                if (board.getPiece(new ChessPosition(row, col)) == null){
-                    possibleMoves.add(new ChessMove(new ChessPosition(position.getRow(),position.getColumn()), new ChessPosition(row,col), null));
-                    row += directions[0];
-                    col += directions[1];
+                if (row + moveDirection == promotionRow) {
+                    //promote
                 }
-                else if (board.getPiece(new ChessPosition(row, col)) != null){// piece was found where we want to go
-                    ChessPiece piece_in_way = board.getPiece(new ChessPosition(row, col));
-                    if (piece_in_way.getTeamColor() != board.getPiece(position).getTeamColor()){// if piece in our way is opposite color, that spot is a valid move (capture), then end checking in this direction
-                        possibleMoves.add(new ChessMove(new ChessPosition(position.getRow(),position.getColumn()), new ChessPosition(row,col), null));
-                    }
-                    break;
+
+                if (row == startingRow && board.getPiece(new ChessPosition(row + moveDirection + moveDirection, col)) == null) {//if on starting row and first space was empty, check next space to validate double move forward
+                    possibleMoves.add(new ChessMove(new ChessPosition(row, col), new ChessPosition(row + moveDirection + moveDirection, col), null));
+                }
+            }
+        }
+
+
+        // Attack checks
+        if (row >= 2 && row <= 7) {
+            if (board.getPiece(new ChessPosition(row + attackPositionLeft[0], col + attackPositionLeft[1])) != null) {// some piece is diagonal left to us, check type to see if we can attack it
+                if (board.getPiece(new ChessPosition(row + attackPositionLeft[0], col + attackPositionLeft[1])).getTeamColor() != board.getPiece(position).getTeamColor()) {// if piece in our way is opposite color, that spot is a valid move (capture)
+                    possibleMoves.add(new ChessMove(new ChessPosition(row, col), new ChessPosition(row + attackPositionLeft[0], col + attackPositionLeft[1]), null));
+                }
+            }
+            if (board.getPiece(new ChessPosition(row + attackPositionRight[0], col + attackPositionRight[1])) != null) {// some piece is diagonal right to us, check type to see if we can attack it
+                if (board.getPiece(new ChessPosition(row + attackPositionRight[0], col + attackPositionRight[1])).getTeamColor() != board.getPiece(position).getTeamColor()) {// if piece in our way is opposite color, that spot is a valid move (capture)
+                    possibleMoves.add(new ChessMove(new ChessPosition(row, col), new ChessPosition(row + attackPositionRight[0], col + attackPositionRight[1]), null));
                 }
             }
         }
